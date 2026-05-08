be179d1ef7a777d52167a19605933bffb1eb6950
web-g <webgyorc@gmail.com>
2026-05-09 01:26:24 +0530

    Branding: Upgrade to Lead CRM V4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,16 +10,15 @@ export default defineConfig({
       registerType: 'autoUpdate',
       disable: false, 
       manifest: {
-        name: 'Lead CRM Pro V3',
-        short_name: 'CRM V3',
+        name: 'Lead CRM  V4',
+        short_name: 'CRM V4',
         start_url: '.',
         display: 'standalone',
         theme_color: '#0F172A',
         background_color: '#0F172A',
-        icons: [
-          // Using the names you have in your public folder now
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+       icons: [
+          { src: 'icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512x512.png', sizes: '512x512', type: 'image/png' }
         ]
       },
       workbox: {
